4b770a53b2c9bdcfd1b91e4996f496102126b97f
Sienna <sstephens@pymblelc.nsw.edu.au>
2022-07-20 20:09:17 +1000

    sync to submit

--- assessmentAI.js
+++ assessmentAI.js
@@ -12,6 +12,7 @@ const buttonAnalyse = document.getElementById("btnAnalyse");
 const webcam = new Webcam(webcamElement, "user", canvasElement);
 
 let results = document.getElementById("AnalyseText");
+let determineresults = document.getElementById("Determine")
 
 let myButton = document.getElementById("btnAnalyse");
 let myText = document.getElementById("myText");
@@ -56,59 +57,13 @@ canvasElement.toBlob(function (data) {
 
 // Analyse Face in the camera and see if it meets the subject criteria
 buttonAnalyse.addEventListener("click", function () {
+  results.innerHTML = '';
+  faces = [];
   canvasElement.toBlob(function (blob) {
     //this takes the data from the canvas and the data is turned into a blob
     ImageAPI.analyseFacesBlob(blob, (data) => {
       //the blob is then fed into the API
-      console.log(data);
-      console.log(data[0])
-      // for (let i = 0; i < data.length; i++) {
-      for (person in data) {
-        console.log("person: "+ person);
-        let personList = document.createElement("ul");
-        faces.push(data[person]);
-
-        //Moustache or no moustache
-        let hair = data[person].faceAttributes.facialHair.moustache;
-        let hairText = "";
-        if (hair == 1) {
-          hairText = "moustache.";
-        } else {
-          hairText = " no moustache.";
-        }
-        if (data[person].faceAttributes.age > 40) {
-          console.log("Person is older than 40");
-          personList.innerHTML +=
-            "<li>" + data[person].faceAttributes.age + "</li>";
-        }
-        // Glasses or no glasses
-        let glasses = data[person].faceAttributes.glasses;
-        let glassesText = "";
-        if (glasses == "NoGlasses") {
-          glassesText = "no glasses";
-        } else {
-          glassesText = "glasses";
-        }
-
-        let text =
-          "<li>" + "Face " + (parseInt(person) + 1) + "</li>";
-  
-        personList.innerHTML += text;
-  
-        personList.innerHTML += "<li>" + glassesText + "</li>";
-  
-        personList.innerHTML += "<li>" + hairText + "</li>";
-  
-        results.appendChild(personList);
-       
-        // faces.push({
-        //   hair: hair,
-        //   hairText: hairText,
-        //   glasses: glasses,
-        //   glassesText: glassesText,
-        //   finalComment: text,
-        // });
-      }
+      analyseFaces(data);
     });
   });
 });
@@ -118,8 +73,7 @@ buttonAnalyse.addEventListener("click", function () {
 btnDetermine.addEventListener("click", function () {
   let comments = '';
   for (let i = 0; i < faces.length; i++) {
-    console.log(faces[i]);
-    if ((faces[i].faceAttributes.gender == "female" && faces[i].faceAttributes.glasses != "NoGlasses" ) || (faces[i].faceAttributes.gender == "male" && faces[i].faceAttributes.age < 30 )) {
+    if (faceisSuspicious(faces[i])) {
       faces[i].finalComment =
         "<p>" + "Face" + (i + 1) + " Matches the suspect description in the database." + "</p>";
     } else {
@@ -128,77 +82,71 @@ btnDetermine.addEventListener("click", function () {
     }
     comments += faces[i].finalComment;
   }
-  results.innerHTML = comments;
+  determineresults.innerHTML = comments;
 });
-/*
-// This uses the analysis determined by the computer and determines whether it meets the suspect decription.
-btnDetermine.addEventListener("click", function () {
-  personList.innerHTML = "";
-  console.log(faces);
-  for (let i = 0; i < faces.length; i++) {
-    if (faces[i].hair == 1 && faces[i].glasses != "NoGlasses") {
-      faces[i].finalComment =
-        "<p>" + "Face" + (person.id + 1) + "Matches the suspect description in the database." + "</p>";
+
+
+
+function analyseFaces(data) {
+  console.log(data);
+  console.log(data[0]);
+  // for (let i = 0; i < data.length; i++) {
+  for (person in data) {
+    console.log("person: "+ person);
+    let personList = document.createElement("ul");
+    faces.push(data[person]);
+
+    //Moustache or no moustache
+    let hair = data[person].faceAttributes.facialHair.moustache;
+    let hairText = "";
+    if (hair == 1) {
+      hairText = "moustache.";
     } else {
-      faces[i].finalComment =
-        "<p>" + "face" + "Does not match the suspect decription in the database" + "<p>";
+      hairText = " no moustache.";
     }
-    results.innerHTML += faces[i].finalComment;
+    if (data[person].faceAttributes.age > 40) {
+      console.log("Person is older than 40");
+      personList.innerHTML +=
+        "<li>" + data[person].faceAttributes.age + "</li>";
+    }
+    // Glasses or no glasses
+    let glasses = data[person].faceAttributes.glasses;
+    let glassesText = "";
+    if (glasses == "NoGlasses") {
+      glassesText = "no glasses";
+    } else {
+      glassesText = "glasses";
+    }
+
+    let text =
+      "<li>" + "Face " + (parseInt(person) + 1) + "</li>";
+
+    personList.innerHTML += text;
+
+    personList.innerHTML += "<li>" + glassesText + "</li>";
+
+    personList.innerHTML += "<li>" + hairText + "</li>";
+
+    results.appendChild(personList);
   }
-});
-*/
-// let myImage = document.getElementById("myPhoto");
-// let imageURL = myImage.src;
+  
+}
 
-// var faces = []; //undefined
+function faceisSuspicious(face) {
 
-//       //Moustache or no moustache
-//       let hair = data[person].faceAttributes.facialHair.moustache;
-//       let hairText = "";
-//       if (hair == 1) {
-//         hairText = "moustache.";
-//       } else {
-//         hairText = " no moustache.";
-//       }
-//       if (data[person].faceAttributes.age > 40) {
-//         console.log("Person is older than 40");
-//         personList.innerHTML +=
-//           "<li>" + data[person].faceAttributes.age + "</li>";
-//       }
-//       // Glasses or no glasses
-//       let glasses = data[person].faceAttributes.glasses;
-//       let glassesText = "";
-//       if (glasses == "NoGlasses") {
-//         glassesText = "no glasses";
-//       } else {
-//         glassesText = "glasses";
-//       }
+  let details = face.faceAttributes;
 
-//       // let text =
-//       //   "<li>" +
-//       //   // "Face " +
-//       //   // (i + 1) +
-//       //   glassesText +
-//       //   hairText +
-//       //   "</li>";
+  if (details.gender == "female" && details.glasses != "NoGlasses" ) {
+    return true;
+  }
+  
+  if (details.facialHair == "moustache" && details.age < 25 && details.facialHair =="beard") {
+    return true;
+  }
+  
+  if (details.gender == "male" && details.age < 30 ) {
+    return true;
+  }
 
-//       // personList.innerHTML += text;
-
-//       personList.innerHTML += "<li>" + glassesText + "</li>";
-
-//       personList.innerHTML += "<li>" + hairText + "</li>";
-
-//       faces.push({
-//         hair: hair,
-//         hairText: hairText,
-//         glasses: glasses,
-//         glassesText: glassesText,
-//         // finalComment: text,
-//       });
-
-//       myText.innerHTML += "<p>" + person + "</p>";
-//       // Put the person list on the screen
-//       myText.append(personList);
-//     }
-//   });
-// });
+  return false;
+}
